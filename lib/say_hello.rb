bd28f94d88004a40731d2b44c8d3bea0243e2e69
EC2 Default User <ec2-user@ip-172-31-2-243.us-east-2.compute.internal>
2020-10-22 13:41:15 +0000

    Done.

---
+++ lib/say_hello.rb
@@ -0,0 +1,7 @@
+def say_hello(name="Gabriela",language="Ruby")
+    puts"Hello Ruby Programmer!"
+end
+
+# I call on the method, say_hello, and give it the string "Gabriela" 
+say_hello("Gabriela")
+
